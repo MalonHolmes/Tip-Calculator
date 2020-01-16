52e1f92a702ff04d786811a4fefb0bd5701c343b
Malon Holmes <itsmalonholmes@gmail.com>
2020-01-16 10:55:28 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by JAMIE HOLMES on 1/16/20.
+//  Copyright © 2020 Malon Holmes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
